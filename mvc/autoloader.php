e006f403b6fc620d25241d15ea569feef4b5ad76
Arpit jayswal <arpitjayswal210@gmail.com>
2025-12-25 21:56:12 +0530

    adding git ignore file

--- mvc/autoloader.php
+++ mvc/autoloader.php
@@ -1,6 +1,11 @@
 <?php
 declare(strict_types=1);
 spl_autoload_register(function($classPath){
+    // spl_autoload_register ka ek sidhhant hai jab bhi class call hogi tab run hoga  automatic
+//     Ye PHP ko bolta hai:
+
+// “Jab bhi koi class use ho aur wo class abhi loaded na ho,
+// toh is function ko automatically call kar dena.”
     // echo $classPath."<br>";
     $path = __DIR__.DIRECTORY_SEPARATOR.$classPath.'.php';
     // echo $path."<br>";
--- phpApp/src/public/index.php
+++ phpApp/src/public/index.php
@@ -1,3 +1,9 @@
 <?php
-echo "<h1 style='color:yellow'>Jay shri ganeshay namah:::</h1>";
-phpinfo();
+require_once "autoloader.php";
+// require_once "./testClass.php";
+// echo "<h1 style='color:yellow'>Jay shri ganeshay namah:::</h1>";
+// echo __DIR__;
+$a = new public\testClass();
+die;
+
+
